9bbdbb80eb040c85453f6a56d7429a957f526d67
unknown <mtruong@decorauto.com>
2018-06-14 11:24:47 -0700

    added setup method for all logintests

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,9 +1,25 @@
 package com.qa.tests;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+	}
+	
 	//test 1
 	@Test
 	public void test1() {
